086bf47515fb6e9ec6d649dbb69ab8056cddc76a
Sarah-Bressan <sarah.bressan@escola.pr.gov.br>
2025-08-14 12:01:37 -0300

    Add files via upload

---
+++ Flascards---Projeto-3-main/Flascards---Projeto-3-main/Flascards---Projeto-3-main (1)/Flascards---Projeto-3-main/Flash Card - Projeto/Flascards---Projeto-3-main/Flash Card - Projeto/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Geografia',
+    'Quais são os cinco membros permanentes do Conselho de Segurança da ONU que possuem poder de veto?',
+    'China, Rússia, Reina Unido, França e Estados Unidos'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os seis continentes?',
+    'América, Africa, Europa, Ásia, Oceania e Antártida'
+)
+
+criaCartao(
+    'Biologia',
+    'O Reino Plantae é dividido em quatro grupos, quais são eles?',
+    'Briófitas, Pteridófitas, Angiospermas e Gimnospermas'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são os níveis de organização do corpo humano?',
+    'Célula, Tecido, Órgão, Sistema e Organismo'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula de Pitágoras?',
+    'A fórmula de Pitágoras é A²=B²+C²'
+)
+
+criaCartao(
+    'Inglês',
+    'Quais são os pronomes para a terceira pessoa do singular em Inglês?',
+    'It, She & He'
+)
